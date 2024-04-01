15019e2ded0e80d1110719c76b93de69483272e6
zhengzhang <zhengzhang@ruijie.com.cn>
2024-04-01 16:59:06 +0800

    train_classify

---
+++ yolo/train_classify/cls_train.py
@@ -0,0 +1,28 @@
+from ultralytics import YOLO
+import cv2
+from pathlib import Path
+
+
+# 模型路径
+model_path = Path('../model').resolve()
+# 资源路径
+data_path = Path('./mnist160').resolve()
+
+
+if __name__ == '__main__':
+
+    print(model_path)
+    print(data_path)
+    # Load a model
+    model = YOLO(model_path /'yolov8n-cls.pt',task="classify")  # load a pretrained model (recommended for training)
+
+    # Train the model
+    # results = model.train(data='mnist160', epochs=100, imgsz=64)
+    # results = model.train(data='./YoloClsDataset4060.yaml', project="./", epochs=100, imgsz=64)
+    # 230926_1556_ 分类任务可能并不支持yaml文件的方式
+    # dataPath = r"D:\_230711_learnArchive\fry_course_materials\240206_Yolov8\ultralytics_fry\_Part03_SelfDataset\_01_TrainClassify\mnist160"
+    results = model.train(data=data_path, project="./", epochs=100, imgsz=64)
+    
+
+
+
